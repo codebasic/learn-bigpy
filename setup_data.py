4aeb8591cc66661019ade50c04e9a9b70477a961
Lee Seongjoo <seongjoo@codebasic.co>
2017-02-23 14:30:50 +0900

    폴더 생성 선택 추가

--- setup_data.py
+++ setup_data.py
@@ -1,6 +1,10 @@
 import os
 import sys
 import subprocess
+import platform
+import argparse
+import time
+
 import requests
 
 def download(url, filepath, chunksize=100000, overwrite=False):
@@ -8,30 +12,64 @@ def download(url, filepath, chunksize=100000, overwrite=False):
         print('{} 파일이 존재합니다. 다운로드를 중단합니다.'.format(filepath))
         sys.exit(1)
 
-    res = requests.get(url)
+    res = requests.get(url, stream=True)
     res.raise_for_status()
 
     file_size = int(res.headers.get('Content-Length'))
-    print('다운로드 받을 파일 크기: {:1f} MB'.format(file_size / 2**20))
+    print('다운로드 받을 파일 크기: {:.1f} MB'.format(file_size / 2**20))
     with open(filepath, 'wb') as target_file:
-        print('다운로드 진행 중 ...')
+        total = 0
         for chunk in res.iter_content(chunksize):
             if chunk:
                 target_file.write(chunk)
-                #print('{:%}'.format(len(chunk)/file_size), end='\r')
+                total += len(chunk)
+                print('{:.0%}'.format(total/file_size), end='\r')
+                time.sleep(0.01)
+        print('')
 
 def main():
-    url = 'https://storage.googleapis.com/codebasic-website-159307.appspot.com/data/bigpy_data.zip'
-    target = 'data.zip'
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--format_dir', dest='format_dir', action='store_true', default=False)
+    parser.add_argument(
+        '--dirpath', dest='dirpath')
+    args = parser.parse_args()
 
-    # 1. download
+    # 사용자 경로 설정
+    dirpath = '.' if not args.dirpath else args.dirpath
+    if not os.path.exists(dirpath):
+        print('오류! {} 경로가 존재하지 않습니다.'.format(os.path.abspath(dirpath)))
+        return sys.exit(1)
+
+    # 폴더 구성 설정
+    if args.format_dir:
+        # 사용자가 대상 경로를 지정하는 경우
+        if args.dirpath:
+            dirpath = args.dirpath
+        # 사용자가 대상 경로를 지정하지 않는 경우
+        else:
+            environ_var = 'HOMEPATH' if platform.system() == 'Windows' else 'HOME'
+            userhome = os.getenv(environ_var)
+            dirpath = os.path.join(userhome, 'Documents')
+
+        # create directory
+        dirpath = os.path.join(dirpath, 'bigpy')
+        if not os.path.exists(dirpath):
+            os.makedirs(dirpath)
+            print('{} 경로 생성'.format(dirpath))
+
+    print('경로: {}'.format(os.path.abspath(dirpath)))
+
+    # 2. download data
+    url = 'https://storage.googleapis.com/codebasic-website-159307.appspot.com/data/bigpy_data.zip'
+    target = os.path.join(dirpath, 'data.zip')
     download(url, target)
 
-    # 2. extract zip file
+    # 3. extract zip file
     # 압축 해제
     print('다운로드 받은 파일 압축 해제', end=' ... ')
-    command_to_extract_zip = 'python -m zipfile -e {} .'.format(target).split()
-    subprocess.run(command_to_extract_zip, check=True)
+    command_to_extract_zip = 'python -m zipfile -e {} {}'.format(target, dirpath)
+    subprocess.run(command_to_extract_zip.split(), check=True)
     print('완료')
 
 if __name__ == '__main__':
